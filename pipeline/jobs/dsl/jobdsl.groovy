b8ef3df6a92dca991348946136b203f5f11a52a8
jonny sywulak <jonny@stelligent.com>
2015-08-17 21:50:06 -0700

    checkpoints job seed implementation
    figuring out how tthe aws codepipeline plugin was a fun way to spend a monday night. I am not sure this is 100% correct but I am 100% sure that I don't want to have to start from scratch so I'm committing this.

--- pipeline/jobs/dsl/jobdsl.groovy
+++ pipeline/jobs/dsl/jobdsl.groovy
@@ -5,9 +5,33 @@ jobs.each { currentJob ->
     job {
       println "configuring ${currentJob}"
       name "${currentJob}-dsl"
-      scm {
-        // how do we get the AWS plugin in here?
-      }
+
+      configure { job ->
+        scm = (job / scm)
+        scm.attributes['class'] = 'com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelineSCM'
+        scm.attributes['plugin'] = 'codepipeline @0.2'
+
+        (scm / 'clearWorkspace').setValue("true")
+        (scm / 'actionTypeCategory').setValue("build")
+        (scm / 'actionTypeProvider').setValue(currentJob)
+        (scm / 'actionTypeVersion').setValue("1")
+
+
+        (scm / 'model' / 'region').setValue("us-east-1")
+        (scm / 'model' / 'compressionType').setValue("None")
+        (scm / 'model' / 'actionTypeCategory').setValue("Build")
+
+        (scm / 'model' / 'outputBuildArtifacts' / 'compressionType ').setValue("None")
+        (scm / 'model' / 'outputBuildArtifacts' / 'com.amazonaws.services.codepipeline.model.Artifact' / 'name').setValue("MyAppBuild")
+        (scm / 'model' / 'outputBuildArtifacts' / 'com.amazonaws.services.codepipeline.model.Artifact' / 'location' / 'type').setValue("S3")
+
+        pub = (job / publishers / "com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher")
+
+        pub.attributes['class'] = 'com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher'
+        pub.attributes['plugin'] = 'codepipeline @0.2'
+
+        (pub / buildOutputs / "com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher_-OutputTuple" / "outputString").setValue("")
+       }
       triggers {
         scm("* * * * *")
       }
@@ -17,10 +41,5 @@ jobs.each { currentJob ->
       wrappers {
         rvm("2.2.2")
       }
-      publishers {
-        // Need publisher for AWS plugin here too
-      }
     }
   }
-}
-
