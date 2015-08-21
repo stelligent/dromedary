96082c90e50f8376f7b7c879b4733fd3971207b1
jonny sywulak <jonny@stelligent.com>
2015-08-20 21:35:37 -0700

    fixes errors with job seed
    setting attributes in a configure block was a pain to figure out. proper syntax seems to be:
    
    (path / to / node).@nameOfAttribute = 'whateverValueYouDesire'

--- pipeline/jobs/dsl/jobdsl.groovy
+++ pipeline/jobs/dsl/jobdsl.groovy
@@ -2,44 +2,42 @@ def jobs =  [ "unit-test", "static-analysis", "acceptance-test"]
 
 jobs.each { currentJob ->
 
-    job {
-      println "configuring ${currentJob}"
-      name "${currentJob}-dsl"
+  job {
+    println "configuring ${currentJob}"
+    name "${currentJob}-dsl"
 
-      configure { job ->
-        scm = (job / scm)
-        scm.attributes['class'] = 'com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelineSCM'
-        scm.attributes['plugin'] = 'codepipeline @0.2'
+    configure { job ->
+      scm = (job / scm)
 
-        (scm / 'clearWorkspace').setValue("true")
-        (scm / 'actionTypeCategory').setValue("build")
-        (scm / 'actionTypeProvider').setValue(currentJob)
-        (scm / 'actionTypeVersion').setValue("1")
+      (scm / 'clearWorkspace').setValue("true")
+      (scm / 'actionTypeCategory').setValue("build")
+      (scm / 'actionTypeProvider').setValue(currentJob)
+      (scm / 'actionTypeVersion').setValue("1")
 
 
-        (scm / 'model' / 'region').setValue("us-east-1")
-        (scm / 'model' / 'compressionType').setValue("None")
-        (scm / 'model' / 'actionTypeCategory').setValue("Build")
+      (scm / 'model' / 'region').setValue("us-east-1")
+      (scm / 'model' / 'compressionType').setValue("None")
+      (scm / 'model' / 'actionTypeCategory').setValue("Build")
 
-        (scm / 'model' / 'outputBuildArtifacts' / 'compressionType ').setValue("None")
-        (scm / 'model' / 'outputBuildArtifacts' / 'com.amazonaws.services.codepipeline.model.Artifact' / 'name').setValue("dromedary")
-        (scm / 'model' / 'outputBuildArtifacts' / 'com.amazonaws.services.codepipeline.model.Artifact' / 'location' / 'type').setValue("S3")
+      (scm / 'model' / 'outputBuildArtifacts' / 'compressionType ').setValue("None")
+      (scm / 'model' / 'outputBuildArtifacts' / 'com.amazonaws.services.codepipeline.model.Artifact' / 'name').setValue("dromedary")
+      (scm / 'model' / 'outputBuildArtifacts' / 'com.amazonaws.services.codepipeline.model.Artifact' / 'location' / 'type').setValue("S3")
 
-        pub = (job / publishers / "com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher")
+      (job / scm).@class = 'com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelineSCM'
+      (job / scm).@plugin = 'codepipeline@0.2'
 
-        pub.attributes['class'] = 'com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher'
-        pub.attributes['plugin'] = 'codepipeline@0.2'
+      pub = (job / publishers / "com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher")
 
-        (pub / buildOutputs / "com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher_-OutputTuple" / "outputString").setValue("")
-       }
-      triggers {
-        scm("* * * * *")
-      }
-      steps {
-        shell("pipeline/jobs/scripts/${currentJob}.sh")
-      }
-      wrappers {
-        rvm("2.2.2")
-      }
+      (pub).@class = 'com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher'
+      (pub).@plugin = 'codepipeline@0.2'
+      
+      (pub / buildOutputs / "com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher_-OutputTuple" / "outputString").setValue("")
+     }
+    triggers {
+      scm("* * * * *")
+    }
+    steps {
+      shell("pipeline/jobs/scripts/${currentJob}.sh")
     }
   }
+}
