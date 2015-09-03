a1b4e98c204e1f5457fcb777a5202a0368d74103
Vincent Rivellino <vincent.rivellino@stelligent.com>
2015-09-03 12:50:40 -0400

    Use environment.sh in pipeline

--- pipeline/jobs/scripts/build.sh
+++ pipeline/jobs/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 npm install
 gulp dist
-artifact_path="dromedary-$(date +%Y%m%d-%H%M%S)"
+artifact_path="dromedary-$(date +%Y%m%d-%H%M%S).tar.gz"
 aws s3 cp dist/archive.tar.gz s3://$DROMEDARY_S3_BUCKET/$artifact_path
 
-echo "export dromedary_artifact_path=$artifact_path" > pipeline-environment.sh
+echo "export dromedary_artifact_path=$artifact_path" > environment.sh
