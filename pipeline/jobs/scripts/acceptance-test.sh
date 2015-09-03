3398738ef06d7b54b98f9caa93c9573ec778b1ef
Vincent Rivellino <vincent.rivellino@stelligent.com>
2015-09-03 18:06:33 -0400

    Ensure job fails if we cannot query host

--- pipeline/jobs/scripts/acceptance-test.sh
+++ pipeline/jobs/scripts/acceptance-test.sh
@@ -3,6 +3,10 @@
 . environment.sh
 
 dest_host="$(aws cloudformation describe-stacks --stack-name $(basename $dromedary_artifact .tar.gz) --output text --query 'Stacks[0].Outputs[?OutputKey==`PublicDns`].OutputValue')"
+if [ -z "$dest_host" ]; then
+    echo "Empty destination host!" >&2
+    exit 1
+fi
 export TARGET_URL=http://$dest_host:8080
 
 npm install
