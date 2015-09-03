167a70f5a50ee70115e27de9dbe2d524f09a6116
Vincent Rivellino <vincent.rivellino@stelligent.com>
2015-09-03 10:55:14 -0400

    Upload artifact to S3

--- pipeline/jobs/scripts/build.sh
+++ pipeline/jobs/scripts/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -ex
 npm install
 gulp dist
+artifact_path="dromedary-$(date +%Y%m%d-%H%M%S)"
+aws s3 cp dist/archive.tar.gz s3://$DROMEDARY_S3_BUCKET/$artifact_path
+
+echo "export dromedary_artifact_path=$artifact_path" > pipeline-environment.sh
