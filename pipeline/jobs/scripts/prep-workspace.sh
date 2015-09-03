d8f7f14f795b8ddad0708c1e6437dff44c554ba0
Vincent Rivellino <vincent.rivellino@stelligent.com>
2015-09-03 14:08:57 -0400

    Set default region for aws cli

--- pipeline/jobs/scripts/prep-workspace.sh
+++ pipeline/jobs/scripts/prep-workspace.sh
@@ -5,7 +5,12 @@
 rm -rf node_modules dist
 npm install
 
-echo "export dromedary_s3_bucket=$DROMEDARY_S3_BUCKET" > environment.sh
+if [ -n "$AWS_DEFAULT_REGION" ]; then
+    echo "export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" > environment.sh
+else
+    echo "export AWS_DEFAULT_REGION=us-east-1" > environment.sh
+fi
+echo "export dromedary_s3_bucket=$DROMEDARY_S3_BUCKET" >> environment.sh
 echo "export dromedary_vpc_stack_name=$DROMEDARY_VPC_STACK" >> environment.sh
 echo "export dromedary_iam_stack_name=$DROMEDARY_IAM_STACK" >> environment.sh
 echo "export dromedary_ec2_key=$DROMEDARY_EC2_KEY" >> environment.sh
