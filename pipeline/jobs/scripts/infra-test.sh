8e4b146645745a77fdc5749714bd82512839d9d9
jonny sywulak <jonny@stelligent.com>
2015-09-04 02:53:00 -0700

    moves logic from jenkins job into script for infra test

--- pipeline/jobs/scripts/infra-test.sh
+++ pipeline/jobs/scripts/infra-test.sh
@@ -1,1 +1,6 @@
-echo hello world
+. ./environment.sh
+gem install rspec aws-sdk
+pushd test-infra
+export dromedary_security_group="$(aws cloudformation describe-stack-resources --region us-east-1 --stack-name $dromedary_app_stack_name  --query StackResources[?LogicalResourceId==\`InstanceSecurityGroup\`].PhysicalResourceId --output text)"
+rspec
+popd
