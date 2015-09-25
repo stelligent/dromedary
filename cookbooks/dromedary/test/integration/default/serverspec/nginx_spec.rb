13fd5aa3668920a1064065d15bc7db6ec9eb5760
jonny sywulak <jonny@stelligent.com>
2015-09-24 17:02:06 -0700

    adds (disabled) code to test ssl

--- cookbooks/dromedary/test/integration/default/serverspec/nginx_spec.rb
+++ cookbooks/dromedary/test/integration/default/serverspec/nginx_spec.rb
@@ -13,9 +13,16 @@ end
 
 describe port(80) do
   it { should be_listening }
+  # it { should_not be_listening }
+end
+
+describe port(443) do
+  it { should_not be_listening }
+  # it { should be_listening }
 end
 
 # TODO Use a gem for this instead of fork & exec'ing curl
 describe command('curl -s http://localhost') do
+# describe command('curl --insecure -s https://localhost') do
   its(:stdout) { should match /Dromedary/ }
 end
