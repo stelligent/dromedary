d41df6ea526ffd1605ff8e6241faa59e8d367569
Paul Duvall <paul.duvall@stelligent.com>
2015-09-26 18:38:08 -0400

    Update security_group_spec.rb
    Modified to check for port 22 rather than port 8080 and also fail if cidr block is wide open

--- test-infra/spec/security_group_spec.rb
+++ test-infra/spec/security_group_spec.rb
@@ -1,7 +1,7 @@
 require 'aws-sdk'
 
 describe('dromedary_security_group') do
-  it 'will allow all traffic on 8080' do
+  it 'will not allow all traffic on 22' do
     ec2 = Aws::EC2::Client.new(region: 'us-east-1')
     sg = ENV["dromedary_security_group"]
 
@@ -9,18 +9,18 @@ describe('dromedary_security_group') do
 
     group = ec2.describe_security_groups(filters: [{name: "group-id", values: [ sg ], }, ]).security_groups.first
 
-    eightyeighty = group.ip_permissions.select do |perm|
-      perm.from_port == 8080
+    twentytwo = group.ip_permissions.select do |perm|
+      perm.from_port == 22
     end
 
-    expect(eightyeighty).to be
-    expect(eightyeighty.size).to eq 1
+    expect(twentytwo).to be
+    expect(twentytwo.size).to eq 1
 
-    expect(eightyeighty.first.ip_ranges).to be
-    expect(eightyeighty.first.ip_ranges.size).to eq 1
+    expect(twentytwo.first.ip_ranges).to be
+    expect(twentytwo.first.ip_ranges.size).to eq 1
 
-    cidr = eightyeighty.first.ip_ranges.first.cidr_ip
+    cidr = twentytwo.first.ip_ranges.first.cidr_ip
 
-    expect(cidr).to eq "0.0.0.0/0"
+    expect(cidr).not_to eq "0.0.0.0/0"
   end 
 end
