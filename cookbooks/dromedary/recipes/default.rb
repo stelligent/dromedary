6bfb8e405424f93c1204a527e7c5bc8c9e91c2a3
Vincent Rivellino <vincent.rivellino@stelligent.com>
2015-10-02 15:46:04 -0400

    Fix merge conflict that got committed

--- cookbooks/dromedary/recipes/default.rb
+++ cookbooks/dromedary/recipes/default.rb
@@ -7,14 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-<<<<<<< HEAD
-include_recipe 'dromedary::nginx_config'
-# include_recipe 'dromedary::ssl_nginx_config'
-include_recipe 'dromedary::nodejs'
-include_recipe 'dromedary::yum_packages'
-=======
 # include_recipe 'dromedary::nginx_config'
 include_recipe 'dromedary::ssl_nginx_config'
 
 include_recipe 'dromedary::install_dromedary'
->>>>>>> origin/master
