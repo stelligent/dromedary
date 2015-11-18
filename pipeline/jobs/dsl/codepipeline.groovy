d1ede751cfbb1a8a295b61af29f549d3a1bc3ed7
jonny sywulak <jonny@stelligent.com>
2015-11-17 21:33:17 -0800

    adds missing fields

--- pipeline/jobs/dsl/codepipeline.groovy
+++ pipeline/jobs/dsl/codepipeline.groovy
@@ -8,14 +8,19 @@ job('example') {
 
 configure { project ->
   project.remove(project / scm) // remove the existing 'scm' element
+
   project / scm(class: 'com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelineSCM', plugin: 'codepipeline@0.8') {
     clearWorkspace true 
     actionTypeCategory 'Build'
     actionTypeProvider "JenkinsJPSTUE564bc1e4"
-    projectName currentJob
+    projectName 'example'
+    awsAccessKey ''
+    awsSecretKey ''
     actionTypeVersion 1
+    proxyHost ''
     proxyPort 0
     region "us-east-1"
+    awsClientFactory {}
   }
   project.remove(project / publishers)
   project / publishers / "com.amazonaws.codepipeline.jenkinsplugin.AWSCodePipelinePublisher"(plugin:'codepipeline@0.8') {
@@ -24,6 +29,7 @@ configure { project ->
         outputString ""
         }
       }
+    awsClientFactory {}
     }
   }
 }
