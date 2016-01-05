72c81d875d9d87b9c973da766b914fa87ae2f35c
“PaulDuvall” <paul.duvall@stelligent.com>
2016-01-05 15:36:02 -0500

    add new codepipeline cfn for codedeploy - placeholder copy

--- bin/cfn-bootstrap.sh
+++ bin/cfn-bootstrap.sh
@@ -21,8 +21,12 @@ jenkins_stack_name="$stack_basename-jenkins"
 eni_stack_name="$stack_basename-eni"
 ddb_stack_name="$stack_basename-ddb"
 pipeline_stack_name="$stack_basename-pipeline"
+pipeline_deploy_stack_name="$stack_basename-pipeline-deploy"
+
 
 echo The value of arg pipeline_stack_name = $pipeline_stack_name
+echo The value of arg pipeline_deploy_stack_name = $pipeline_deploy_stack_name
+
 
 aws cloudformation create-stack \
     --stack-name $ddb_stack_name \
@@ -72,4 +76,4 @@ echo "export dromedary_jenkins_stack_name=$jenkins_stack_name" >> "$ENVIRONMENT_
 echo "export dromedary_eni_stack_name=$eni_stack_name" >> "$ENVIRONMENT_FILE"
 echo "export dromedary_ec2_key=$DROMEDARY_EC2_KEY" >> "$ENVIRONMENT_FILE"
 echo "export dromedary_pipeline_stack_name=$pipeline_stack_name" >> "$ENVIRONMENT_FILE"
-
+echo "export dromedary_pipeline_codedeploy_stack_name=$pipeline_deploy_stack_name" >> "$ENVIRONMENT_FILE"
--- bin/codepipeline-create.sh
+++ bin/codepipeline-create.sh
@@ -57,15 +57,21 @@ aws codepipeline create-custom-action-type --cli-input-json "$(generate_cli_json
 aws codepipeline create-custom-action-type --cli-input-json "$(generate_cli_json Test)"
 
 pipelinejson=$(mktemp /tmp/dromedary-pipeline.json.XXXX)
+pipelinedeployjson=$(mktemp /tmp/dromedary-pipeline-deploy.json.XXXX)
 pipeline_name="Dromedary$(echo $dromedary_hostname | tr '[[:lower:]]' '[[:upper:]]')"
 
-cp "$script_dir/../pipeline/cfn/codepipeline.json" $pipelinejson
-
+cp "$script_dir/../pipeline/cfn/codepipeline-cfn.json" $pipelinejson
+cp "$script_dir/../pipeline/cfn/codepipeline-codedeploy.json" $pipelinedeployjson
 
 sed s/DromedaryJenkins/$dromedary_custom_action_provider/g $pipelinejson > $pipelinejson.new && mv $pipelinejson.new $pipelinejson
 sed s/DromedaryPipelineName/$pipeline_name/g $pipelinejson > $pipelinejson.new && mv $pipelinejson.new $pipelinejson
 sed s,arn:aws:iam::123456789012:role/AWS-CodePipeline-Service,$codepipeline_role_arn,g $pipelinejson > $pipelinejson.new && mv $pipelinejson.new $pipelinejson
 sed s/codepipeline-us-east-1-XXXXXXXXXXX/$dromedary_s3_bucket/g $pipelinejson > $pipelinejson.new && mv $pipelinejson.new $pipelinejson
+
+sed s/DromedaryJenkins/$dromedary_custom_action_provider/g $pipelinedeployjson > $pipelinedeployjson.new && mv $pipelinedeployjson.new $pipelinedeployjson
+sed s/DromedaryPipelineName/$pipeline_name/g $pipelinedeployjson > $pipelinedeployjson.new && mv $pipelinedeployjson.new $pipelinedeployjson
+sed s,arn:aws:iam::123456789012:role/AWS-CodePipeline-Service,$codepipeline_role_arn,g $pipelinedeployjson > $pipelinedeployjson.new && mv $pipelinedeployjson.new $pipelinedeployjson
+sed s/codepipeline-us-east-1-XXXXXXXXXXX/$dromedary_s3_bucket/g $pipelinedeployjson > $pipelinedeployjson.new && mv $pipelinedeployjson.new $pipelinedeployjson
 
 mygithubtoken=$1
 mygithubuser=$2
@@ -75,9 +81,12 @@ echo The value of variable mygithubtoken = $mygithubtoken
 echo The value of variable mygithubuser = $mygithubuser 
 echo The value of variable mybranch = $mybranch
 echo The value of variable dromedary_pipeline_stack_name = $dromedary_pipeline_stack_name 
-
+echo The value of variable dromedary_pipeline_codedeploy_stack_name = $dromedary_pipeline_codedeploy_stack_name 
 
 aws cloudformation create-stack --stack-name $dromedary_pipeline_stack_name --template-body file://$pipelinejson --region us-east-1 --disable-rollback --capabilities="CAPABILITY_IAM" --parameters ParameterKey=GitHubToken,ParameterValue=$mygithubtoken ParameterKey=GitHubUser,ParameterValue=$mygithubuser ParameterKey=Branch,ParameterValue=$mybranch
+aws cloudformation create-stack --stack-name $dromedary_pipeline_codedeploy_stack_name --template-body file://$pipelinejson --region us-east-1 --disable-rollback --capabilities="CAPABILITY_IAM" --parameters ParameterKey=GitHubToken,ParameterValue=$mygithubtoken ParameterKey=GitHubUser,ParameterValue=$mygithubuser ParameterKey=Branch,ParameterValue=$mybranch
+
 
 echo "export dromedary_codepipeline=$dromedary_pipeline_stack_name" >> "$ENVIRONMENT_FILE"
+echo "export dromedary_codepipeline_codedeploy=$dromedary_pipeline_codedeploy_stack_name" >> "$ENVIRONMENT_FILE"
 rm -f $pipelinejson
