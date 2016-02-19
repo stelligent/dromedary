e24355734248d4a4a07f603534e82f3ce1e19472
Nicholas DeClario <nicholas.declario@stelligent.com>
2016-02-19 10:06:48 -0500

    Added ZAP AMI creation.  This currently relies on an exisiting VPC to work.

---
+++ zap-ami/roles/zap/files/zap-init.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:             zap
+# Required-Start:       $remote_fs $syslog
+# Required-Stop:        $remote_fs $syslog
+# Default-Start:        2 3 4 5
+# Default-Stop:         
+# Short-Description:    OWASP ZAP Daemon Server
+### END INIT INFO
+
+set -e
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+ZAP=/opt/zap/zap.sh
+ZAP_LOG=/var/log/zap.log
+
+test -x ${ZAP} || exit 0
+
+fetch_state() {
+    if [ ! -f /var/run/zap.run ]; then
+        state=1
+        return
+    fi
+
+    PID=$(cat /var/run/zap.run)
+    [ -n "$(ps aux | grep -i ${PID}.*java.*zap-[0-9].*\.jar)" ] && \
+    {
+        state=0
+        return
+    }
+
+    state=1
+    return 
+}
+
+start_zap() {
+    echo -en "Starting Zap:\t"
+
+    if [ ${state} -eq 0 ]; then 
+        echo "Zap already running; skipping" 1>&2
+        exit 1
+    fi
+    pushd /opt/zap 1 >> /dev/null
+    ${ZAP} -daemon -port 8090 -config api.disablekey=true >> $ZAP_LOG 2>&1 &
+    sleep 2
+    while [ -z "$(tail -n 2 $ZAP_LOG | grep 'ZAP is now listening')" ]; do
+        sleep 1
+    done
+    echo $! > /var/run/zap.run
+    echo "Started"
+}
+
+stop_zap() {
+    echo -en "Stopping Zap\t"
+    if [ ${state} -eq 1 ]; then
+        echo "Zap not running; skipping" 1>&2
+        return
+    fi
+
+    PID=$(cat /var/run/zap.run)
+    kill ${PID}
+    while [ -n "$(ps -p ${PID} -o comm=)" ]; do
+        sleep 1
+    done
+    rm -f /var/run/zap.run
+
+    echo "Stopped"
+}
+
+zap_status() {
+    [ ${state} -eq 0 ] && status="Running" || status="Stopped"
+    echo -en "Status of zap: ${status}\n"
+}
+
+fetch_state 
+
+case "$1" in
+  start)
+    start_zap
+    ;;
+  stop)
+    stop_zap
+    ;;
+  restart)
+    stop_zap
+    fetch_state 
+    start_zap
+    ;;
+  status)
+    zap_status
+    ;;
+  *)
+    echo "Usage: /etc/init.d/zap {start|stop|reload|status}" 2>&1
+    exit 1
+esac
+
+exit 0
+
