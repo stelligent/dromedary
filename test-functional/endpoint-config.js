f955beecdea1a25962b10c721cd0f354de94e41b
Casey Lee <casey.lee@stelligent.com>
2016-02-21 21:29:27 -0800

    Revise tests to:
    * not use urlsync, since it requires a new version of Node.js than lambda provides
    * be async via `request-promise`
    * handle testing of an app with static content in S3 and API behind gateway with 2 different endpoints

--- test-functional/endpoint-config.js
+++ test-functional/endpoint-config.js
@@ -1,24 +1,30 @@
 var expect    = require("chai").expect;
-var request   = require('urllib-sync').request;
+var rp        = require('request-promise');
 var targetUrl = process.env.hasOwnProperty('TARGET_URL') ? process.env.TARGET_URL : 'http://localhost:8080';
 var shaRegex  = /^([0-9a-f]{40}|[0-9]{8}-[0-9]{6})$/;
 
 describe("/config.json", function() {
-  beforeEach(function() {
-    this.resp = JSON.parse(request(targetUrl + '/config.json').data.toString('utf-8'));
-    console.log(this.resp)
+  var resp;
+  beforeEach(function(done) {
+    rp({ uri: targetUrl+'/config.json', json:true})
+        .then(function (data) {
+          resp = data;
+          done();
+        })
+        .catch(function (err) {
+          throw err;
+        });
+
+    console.log(resp)
   });
 
   it("response contains version key", function() {
-    expect(this.resp).to.include.keys('version');
+    expect(resp).to.include.keys('version');
   });
   it("response contains apiBaseurl key", function() {
-    expect(this.resp).to.include.keys('apiBaseurl');
+    expect(resp).to.include.keys('apiBaseurl');
   });
   it("version value matches regex: " + shaRegex.toString(), function() {
-    expect(this.resp.version).to.match(shaRegex);
-  });
-  it("apiBaseurl value matches: ''", function() {
-    expect(this.resp.apiBaseurl).to.match(/^$/);
+    expect(resp.version).to.match(shaRegex);
   });
 });
--- test-functional/endpoint-data.js
+++ test-functional/endpoint-data.js
@@ -1,24 +1,48 @@
 var expect    = require("chai").expect;
-var request   = require('urllib-sync').request;
-
+var rp        = require('request-promise');
 var targetUrl   = process.env.hasOwnProperty('TARGET_URL') ? process.env.TARGET_URL : 'http://localhost:8080';
 
 var expectedNumberOfItems = 3;
 var expectedProperties = ['value', 'color', 'highlight', 'label'];
 
 describe("/data", function() {
-  beforeEach(function() {
-    this.chartData = JSON.parse(request(targetUrl + '/data').data.toString('utf-8'));
+
+  var apiBaseurl;
+  before(function(done) {
+      rp({ uri: targetUrl+'/config.json', json:true})
+          .then(function (data) {
+              if(!data.apiBaseurl || data.apiBaseurl == '/') {
+                  apiBaseurl = targetUrl;
+              } else {
+                  apiBaseurl = data.apiBaseurl;
+              }
+              done();
+          })
+          .catch(function (err) {
+              throw err;
+          });
+  });
+
+  var chartData;
+  beforeEach(function(done) {
+    rp({ uri: apiBaseurl+'/data', json:true})
+        .then(function(data) {
+          chartData = data;
+            done();
+        })
+        .catch (function(err) {
+          throw err;
+        });
   });
 
   it("response has exactly " + expectedNumberOfItems + " items", function() {
-    expect(this.chartData).to.have.length(expectedNumberOfItems);
+    expect(chartData).to.have.length(expectedNumberOfItems);
   });
 
   it("each item has exactly " + expectedProperties.length + " properties", function() {
     var index;
-    for (index = 0; index < this.chartData.length; index++) {
-      expect(Object.keys(this.chartData[index])).to.have.length(expectedProperties.length);
+    for (index = 0; index < chartData.length; index++) {
+      expect(Object.keys(chartData[index])).to.have.length(expectedProperties.length);
     }
   });
 
@@ -26,8 +50,8 @@ describe("/data", function() {
     var itemProperties;
     var itemIndex;
     var propIndex;
-    for (itemIndex = 0; itemIndex < this.chartData.length; itemIndex++) {
-      itemProperties = Object.keys(this.chartData[itemIndex]);
+    for (itemIndex = 0; itemIndex < chartData.length; itemIndex++) {
+      itemProperties = Object.keys(chartData[itemIndex]);
       for (propIndex = 0; propIndex < expectedProperties.length; propIndex++) {
         expect(itemProperties).to.contain(expectedProperties[propIndex]);
       }
@@ -36,22 +60,60 @@ describe("/data", function() {
 });
 
 describe("/data?countsOnly", function() {
-  beforeEach(function() {
-    this.chartData = JSON.parse(request(targetUrl + '/data').data.toString('utf-8'));
-    this.colorCounts = JSON.parse(request(targetUrl + '/data?countsOnly').data.toString('utf-8'));
+  var apiBaseurl;
+  before(function(done) {
+      rp({ uri: targetUrl+'/config.json', json:true})
+          .then(function (data) {
+              if(!data.apiBaseurl || data.apiBaseurl == '/') {
+                  apiBaseurl = targetUrl;
+              } else {
+                  apiBaseurl = data.apiBaseurl;
+              }
+              done();
+          })
+          .catch(function (err) {
+              throw err;
+          });
+  });
+
+  var chartData;
+  var colorCounts;
+  beforeEach(function(done) {
+    var replyCount = 0;
+    rp({ uri: apiBaseurl+'/data', json:true})
+        .then(function(data) {
+          chartData = data;
+          if(++replyCount == 2) {
+              done();
+          }
+        })
+        .catch (function(err) {
+          throw err;
+        });
+
+    rp({ uri: apiBaseurl+'/data', qs:{countsOnly: true}, json:true})
+        .then(function(data) {
+          colorCounts = data;
+          if(++replyCount == 2) {
+              done();
+          }
+        })
+        .catch (function(err) {
+          throw err;
+        });
   });
 
   it("response has exactly " + expectedNumberOfItems + " keys", function() {
-    expect(Object.keys(this.colorCounts)).to.have.length(expectedNumberOfItems);
+    expect(Object.keys(colorCounts)).to.have.length(expectedNumberOfItems);
   });
   it("matches values in /data response", function() {
     var index;
     var color;
     var value;
-    for (index = 0; index < this.chartData.length; index++) {
-      color = this.chartData[index].label.toLowerCase();
-      value = this.chartData[index].value;
-      expect(this.colorCounts[color]).to.equal(value);
+    for (index = 0; index < chartData.length; index++) {
+      color = chartData[index].label.toLowerCase();
+      value = chartData[index].value;
+      expect(colorCounts[color]).to.equal(value);
     }
   });
 });
