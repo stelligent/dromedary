35e74eca9427c0943c3ab206a39fb1179bee38b4
Eric Kascic <eric.kascic@stelligent.com>
2016-02-26 15:23:32 +0000

    post-process results to include a status key at the top level

--- pipeline/jobs/scripts/drom-sec-staticcode-anal.sh
+++ pipeline/jobs/scripts/drom-sec-staticcode-anal.sh
@@ -12,8 +12,11 @@ templates_to_audit=pipeline/cfn/dromedary-master.json
 #templates_to_audit=pipeline/cfn/
 
 cfn_nag --input-json-path ${templates_to_audit} \
-        --output-format json > cfn_nag_results.json
+        --output-format json > cfn_nag_results_raw.json
 cfn_nag_result=$?
+
+cat cfn_nag_results_raw.json | \
+  jq '{ result: (if ([.[]|.file_results.failure_count]|reduce .[] as $item (0; . + $item)) > 0 then "FAIL" else "PASS" end), results: .}' > cfn_nag_results.json
 
 set -e
 aws s3api put-object --bucket dromedary-test-results \
