6066cba36aea0173f0364cd0b6653f150ea797dc
Eric Kascic <eric.kascic@stelligent.com>
2016-02-27 18:56:34 +0000

    sort the violations by the .type field FAIL/WARN

--- pipeline/jobs/scripts/drom-sec-staticcode-anal.sh
+++ pipeline/jobs/scripts/drom-sec-staticcode-anal.sh
@@ -21,7 +21,7 @@ cat cfn_nag_results_raw.json | \
   jq '{ result: (if ([.[]|.file_results.failure_count]|reduce .[] as $item (0; . + $item)) > 0 then "FAIL" else "PASS" end), results: .}' > aggregate_status_cfn_nag_results.json
 
 cat aggregate_status_cfn_nag_results.json | \
-  jq '{ result: .result, results: [.results[]|.filename as $filename|.file_results.violations[]|.filename=$filename]}' > cfn_nag_results.json
+  jq '{ result: .result, results: [.results[]|.filename as $filename|.file_results.violations[]|.filename=$filename]|sort_by(.type)}' > cfn_nag_results.json
 
 aws s3api put-object --bucket dromedary-test-results \
                      --key 'data/cfn_nag_results.json' \
