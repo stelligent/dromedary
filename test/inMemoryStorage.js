ab8fd2243b2baf0d6146f957c2f5d4abb519e117
Casey Lee <casey.lee@stelligent.com>
2016-05-04 13:29:34 -0700

    green for vsp

--- test/inMemoryStorage.js
+++ test/inMemoryStorage.js
@@ -1,7 +1,7 @@
 var expect    = require("chai").expect;
 var InMemStor = require("../lib/inMemoryStorage.js");
 
-var expectedNumberOfItems = 3;
+var expectedNumberOfItems = 4;
 var expectedProperties = ['value', 'color', 'highlight', 'label'];
 var backend = new InMemStor();
 
